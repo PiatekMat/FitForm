e9b697c929ff3fa2eb32769bc3356703c9d8e628
PiatekMat <matikcraftp@gmail.com>
2026-05-07 09:00:00 +0200

    Refactor tu fully flask

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    if request.method == "POST":
+
+        avg_kcal = request.form.get("avg_kcal")
+        avg_burned_kcal = request.form.get("avg_burned_kcal")
+        weight = request.form.get("weight")
+        gender = request.form.get("gender")
+
+        return f"""
+        kcal: {avg_kcal}<br>
+        burned: {avg_burned_kcal}<br>
+        weight: {weight}<br>
+        gender: {gender}
+        """
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
