dffee8a0cb3e96d14a2a9df71cffa174de523b1b
Laura <laura.szczerbowska25@gmail.com>
2026-06-01 16:47:42 +0200

    poprawiony model - config logger i brak danych wrazliwych

--- model_porownanie.py
+++ model_porownanie.py
@@ -1,62 +1,73 @@
 import pandas as pd
 import numpy as np
 import xgboost as xgb
+import lightgbm as lgb
+from catboost import CatBoostRegressor
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GroupKFold
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression, ElasticNet, Ridge
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
 import os
+import logging
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
-from sklearn.model_selection import GroupKFold
+
+# CONFIG LOGGER
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("fitform_backend.log", encoding="utf-8"),  #zapis do pliku (w razie dalszych problemow)
+        logging.StreamHandler()
+    ]
+)
 
 load_dotenv()
-
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+db_url = os.getenv("FITFORM_DB_URL")
 
 try:
-    print(f"Tworzenie połączenia SQLAlchemy z Supabase ({DB_HOST})...")
+    logging.info("Tworzenie połączenia SQLAlchemy z Supabase za pomocą FITFORM_DB_URL")
 
-    # format: postgresql+psycopg2://użytkownik:hasło@host:port/nazwa_bazy
-    db_url = f"postgresql+psycopg2://laura:Laura_FitForm2026@db.ryolatpsrxuvapuaytrd.supabase.co:5432/postgres"
+    if not db_url:
+        raise ValueError("Nie znaleziono zmiennej FITFORM_DB_URL w pliku .env!")
 
     engine = create_engine(db_url)
-
     nazwa_tabeli = "daily_logs"
     query = f"SELECT * FROM {nazwa_tabeli};"
 
-    print("Pobieranie danych za pomocą silnika SQLAlchemy...")
-
     df = pd.read_sql_query(query, engine)
-
-    print(f"Liczba pobranych rekordów: {len(df)}")
-    print(df.head())
+    logging.info(f"Pobrano {len(df)} rekordów.")
 
 except Exception as error:
-    print(f"\n Błąd połączenia: {error}")
-
+    logging.error(f"Błąd połączenia: {error}")
+    exit()
 
 # -------- MODELOWANIE RÓŻNICOWE------
 
 df = df.sort_values(by=['user_id', 'data_wpisu'])
 
+# utworzenie wagi z następnego dnia
 df['waga_jutro'] = df.groupby('user_id')['waga_czczo'].shift(-1)
 
-# Y różnica wagi między dniem dzisiejszym i jutrzejszym
+# Y staje się różnica wagi między jutrem a dzisiaj
 df['roznica_wagi'] = df['waga_jutro'] - df['waga_czczo']
 
 # usuniecie wierszy, dla których nie jest znana różnica (ostatni dzień każdego użytkownika)
 df_clean = df.dropna(subset=['roznica_wagi']).copy()
 
-#brak wagi na czczo, wczesniej negatywnie wplynela na predykcje
+# niewykorzystano wagi na czczo, wczesniej negatywnie wplynela na predykcje
 X = df_clean.drop(['data_wpisu', 'waga_czczo', 'waga_jutro', 'roznica_wagi', 'id', 'user_id'], axis=1, errors='ignore')
 y = df_clean['roznica_wagi']
 
 kolumny_X = X.columns.tolist()
 
 grupy_uzytkownikow = df_clean['user_id']
-cv_grupowe = GroupKFold(n_splits=4) #GroupKFold grwanatuje że każda grupa trafia w całości albo do trenowania, albo do testowania
+cv_grupowe = GroupKFold(n_splits=4)  # GroupKFold grwanatuje że każda grupa trafia w całości albo do trenowania, albo do testowania
 
 # ----------XGBOOST--------------
 model_xgb = xgb.XGBRegressor(
@@ -76,22 +87,19 @@ Walidacja krzyżowa (cv=5) powtarza test 5-krotnie na różnych fragmentach zbio
 stabilniejszą i bardziej obiektywną ocenę błędu RMSE.
 """
 
-
 scores_xgb = cross_val_score(model_xgb, X, y, cv=cv_grupowe, groups=grupy_uzytkownikow, scoring='neg_mean_squared_error')
 
 # obliczamy RMSE
 rmse_scores_xgb = np.sqrt(-scores_xgb)  # zamiast błędu 0.1, zwraca -0.1, dzieki temu wie ze 0.1 wygrywa a nie 50
 
-print("WYNIKI XGBOOST")
-print(f"Błędy RMSE w poszczególnych próbach (kg): {rmse_scores_xgb}")
-print(f"Średni błąd modelu (RMSE): {rmse_scores_xgb.mean():.4f} kg")
+logging.info("--- WYNIKI XGBOOST ---")
+logging.info(f"Błędy RMSE w poszczególnych próbach (kg): {rmse_scores_xgb}")
+logging.info(f"Średni błąd modelu (RMSE): {rmse_scores_xgb.mean():.4f} kg")
 
 error_percentage_xgb = (rmse_scores_xgb.mean() / df_clean['waga_czczo'].mean()) * 100
-print(f"Procentowy błąd predykcji względem wagi: {error_percentage_xgb:.2f}%")
+logging.info(f"Procentowy błąd predykcji względem wagi: {error_percentage_xgb:.2f}%")
 
 # ----------- LIGHTGBM ----------------
-import lightgbm as lgb
-
 model_lgb = lgb.LGBMRegressor(
     n_estimators=50,  # mało drzew, bo mała ilość danych
     max_depth=3,  # płytsze drzewo - zapobiega overfittingowi
@@ -108,16 +116,14 @@ scores_lgb = cross_val_score(model_lgb, X, y, cv=cv_grupowe, groups=grupy_uzytko
 # obliczamy RMSE
 rmse_scores_lgb = np.sqrt(-scores_lgb)  # zwraca błąd jako liczbę ujemną (np. -0.1), aby móc go "maksymalizować".
 # scikit maksymalizuje, więc ważne jest aby w przypadku rmse 0.1 miało większą wartość niż 50 - stąd ta zmiana
-print("\nWYNIKI LIGHTGBM")
-print(f"Błędy RMSE w poszczególnych próbach (kg): {rmse_scores_lgb}")
-print(f"Średni błąd modelu (RMSE): {rmse_scores_lgb.mean():.4f} kg")
+logging.info("--- WYNIKI LIGHTGBM ---")
+logging.info(f"Błędy RMSE w poszczególnych próbach (kg): {rmse_scores_lgb}")
+logging.info(f"Średni błąd modelu (RMSE): {rmse_scores_lgb.mean():.4f} kg")
 
 error_percentage_lgb = (rmse_scores_lgb.mean() / df_clean['waga_czczo'].mean()) * 100
-print(f"Procentowy błąd predykcji względem wagi: {error_percentage_lgb:.2f}%")
+logging.info(f"Procentowy błąd predykcji względem wagi: {error_percentage_lgb:.2f}%")
 
 # -----CATBOOST--------
-from catboost import CatBoostRegressor
-
 model_cat = CatBoostRegressor(
     iterations=200,
     depth=5,
@@ -132,18 +138,14 @@ scores_catboost = cross_val_score(model_cat, X, y, cv=cv_grupowe, groups=grupy_u
 # rmse
 rmse_scores_cat = np.sqrt(-scores_catboost)
 
-print("\nWYNIKI CATBOOST")
-print(f"Błędy RMSE w poszczególnych próbach (kg): {rmse_scores_cat}")
-print(f"Średni błąd modelu (RMSE): {rmse_scores_cat.mean():.4f} kg")
+logging.info("--- WYNIKI CATBOOST ---")
+logging.info(f"Błędy RMSE w poszczególnych próbach (kg): {rmse_scores_cat}")
+logging.info(f"Średni błąd modelu (RMSE): {rmse_scores_cat.mean():.4f} kg")
 
 error_percentage_cat = (rmse_scores_cat.mean() / df_clean['waga_czczo'].mean()) * 100
-print(f"Procentowy błąd predykcji względem wagi: {error_percentage_cat:.2f}%")
+logging.info(f"Procentowy błąd predykcji względem wagi: {error_percentage_cat:.2f}%")
 
 # -----Random Forest-----
-
-from sklearn.ensemble import RandomForestRegressor
-
-# Inicjalizacja modelu
 model_rf = RandomForestRegressor(
     n_estimators=100,
     max_depth=10,
@@ -157,18 +159,14 @@ scores_rf = cross_val_score(model_rf, X, y, cv=cv_grupowe, groups=grupy_uzytkown
 # rmse
 rmse_scores_rf = np.sqrt(-scores_rf)
 
-print("\nWYNIKI RANDOMFOREST")
-print(f"Błędy RMSE w poszczególnych próbach (kg): {rmse_scores_rf}")
-print(f"Średni błąd modelu (RMSE): {rmse_scores_rf.mean():.4f} kg")
+logging.info("--- WYNIKI RANDOMFOREST ---")
+logging.info(f"Błędy RMSE w poszczególnych próbach (kg): {rmse_scores_rf}")
+logging.info(f"Średni błąd modelu (RMSE): {rmse_scores_rf.mean():.4f} kg")
 
 error_percentage_rf = (rmse_scores_rf.mean() / df_clean['waga_czczo'].mean()) * 100
-print(f"Procentowy błąd predykcji względem wagi: {error_percentage_rf:.2f}%")
+logging.info(f"Procentowy błąd predykcji względem wagi: {error_percentage_rf:.2f}%")
 
 # -------REGRESJA LINIOWA---------
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-
 pipeline_lr = Pipeline([
     ('skaler', StandardScaler()),
     ('model_lr', LinearRegression())
@@ -179,16 +177,14 @@ scores_lr = cross_val_score(pipeline_lr, X, y, cv=cv_grupowe, groups=grupy_uzytk
 # RMSE
 rmse_scores_lr = np.sqrt(-scores_lr)
 
-print("\nWYNIKI REGRESJI LINIOWEJ")
-print(f"Błędy RMSE (kg): {rmse_scores_lr}")
-print(f"Średni błąd (RMSE): {rmse_scores_lr.mean():.4f} kg")
+logging.info("--- WYNIKI REGRESJI LINIOWEJ ---")
+logging.info(f"Błędy RMSE (kg): {rmse_scores_lr}")
+logging.info(f"Średni błąd (RMSE): {rmse_scores_lr.mean():.4f} kg")
 
 error_percentage_lr = (rmse_scores_lr.mean() / df_clean['waga_czczo'].mean()) * 100
-print(f"Procentowy błąd: {error_percentage_lr:.2f}%")
+logging.info(f"Procentowy błąd: {error_percentage_lr:.2f}%")
 
 # -------ELASTIC NET----------
-from sklearn.linear_model import ElasticNet
-
 pipeline_en = Pipeline([
     ('skaler', StandardScaler()),
     ('model_en', ElasticNet(alpha=1.0, l1_ratio=0.5, random_state=42))
@@ -202,16 +198,14 @@ scores_en = cross_val_score(pipeline_en, X, y, cv=cv_grupowe, groups=grupy_uzytk
 # obliczanie RMSE
 rmse_scores_en = np.sqrt(-scores_en)
 
-print("\nWYNIKI ELASTIC NET")
-print(f"Błędy RMSE (kg): {rmse_scores_en}")
-print(f"Średni błąd (RMSE): {rmse_scores_en.mean():.4f} kg")
+logging.info("--- WYNIKI ELASTIC NET ---")
+logging.info(f"Błędy RMSE (kg): {rmse_scores_en}")
+logging.info(f"Średni błąd (RMSE): {rmse_scores_en.mean():.4f} kg")
 
 error_percentage_en = (rmse_scores_en.mean() / df_clean['waga_czczo'].mean()) * 100
-print(f"Procentowy błąd: {error_percentage_en:.2f}%")
+logging.info(f"Procentowy błąd: {error_percentage_en:.2f}%")
 
 # ------Ridge Regression---------
-from sklearn.linear_model import Ridge
-
 pipeline_ridge = Pipeline([
     ('skaler', StandardScaler()),
     ('model_ridge', Ridge(alpha=1.0))
@@ -222,16 +216,14 @@ scores_ridge = cross_val_score(pipeline_ridge, X, y, cv=cv_grupowe, groups=grupy
 # obliczanie RMSE
 rmse_scores_ridge = np.sqrt(-scores_ridge)
 
-print("\nWYNIKI RIDGE REGRESSION")
-print(f"Błędy RMSE (kg): {rmse_scores_ridge}")
-print(f"Średni błąd (RMSE): {rmse_scores_ridge.mean():.4f} kg")
+logging.info("--- WYNIKI RIDGE REGRESSION ---")
+logging.info(f"Błędy RMSE (kg): {rmse_scores_ridge}")
+logging.info(f"Średni błąd (RMSE): {rmse_scores_ridge.mean():.4f} kg")
 
 error_percentage_ridge = (rmse_scores_ridge.mean() / df_clean['waga_czczo'].mean()) * 100
-print(f"Procentowy błąd: {error_percentage_ridge:.2f}%")
+logging.info(f"Procentowy błąd: {error_percentage_ridge:.2f}%")
 
 # ------SVM-----------
-from sklearn.svm import SVR
-
 pipeline_svm = Pipeline([
     ('skaler', StandardScaler()),
     ('model_svm', SVR(kernel='rbf', C=10.0, epsilon=0.1))  # rbf pozwala uczyc sie krzywych
@@ -245,16 +237,14 @@ scores_svm = cross_val_score(pipeline_svm, X, y, cv=cv_grupowe, groups=grupy_uzy
 # obliczanie RMSE
 rmse_scores_svm = np.sqrt(-scores_svm)
 
-print("\nWYNIKI SVM")
-print(f"Błędy RMSE (kg): {rmse_scores_svm}")
-print(f"Średni błąd (RMSE): {rmse_scores_svm.mean():.4f} kg")
+logging.info("--- WYNIKI SVM ---")
+logging.info(f"Błędy RMSE (kg): {rmse_scores_svm}")
+logging.info(f"Średni błąd (RMSE): {rmse_scores_svm.mean():.4f} kg")
 
 error_percentage_svm = (rmse_scores_svm.mean() / df_clean['waga_czczo'].mean()) * 100
-print(f"Procentowy błąd: {error_percentage_svm:.2f}%")
+logging.info(f"Procentowy błąd: {error_percentage_svm:.2f}%")
 
 # ---------KNN------------
-from sklearn.neighbors import KNeighborsRegressor
-
 pipeline_knn = Pipeline([
     ('skaler', StandardScaler()),
     ('model_knn', KNeighborsRegressor(n_neighbors=5, weights='distance'))
@@ -266,16 +256,14 @@ scores_knn = cross_val_score(pipeline_knn, X, y, cv=cv_grupowe, groups=grupy_uzy
 # obliczanie RMSE
 rmse_scores_knn = np.sqrt(-scores_knn)
 
-print("\nWYNIKI KNN")
-print(f"Błędy RMSE (kg): {rmse_scores_knn}")
-print(f"Średni błąd (RMSE): {rmse_scores_knn.mean():.4f} kg")
+logging.info("--- WYNIKI KNN ---")
+logging.info(f"Błędy RMSE (kg): {rmse_scores_knn}")
+logging.info(f"Średni błąd (RMSE): {rmse_scores_knn.mean():.4f} kg")
 
 error_percentage_knn = (rmse_scores_knn.mean() / df_clean['waga_czczo'].mean()) * 100
-print(f"Procentowy błąd: {error_percentage_knn:.2f}%")
+logging.info(f"Procentowy błąd: {error_percentage_knn:.2f}%")
 
 # -------DECISION TREE----------
-from sklearn.tree import DecisionTreeRegressor
-
 model_dt = DecisionTreeRegressor(
     max_depth=5,  # jak dużo pytań może zadać drzewo
     min_samples_split=10,  # minimalna liczba próbek, by stworzyć nowy podział
@@ -286,15 +274,16 @@ scores_dt = cross_val_score(model_dt, X, y, cv=cv_grupowe, groups=grupy_uzytkown
 # obliczanie RMSE
 rmse_scores_dt = np.sqrt(-scores_dt)
 
-print("\nWYNIKI DECISION TREE")
-print(f"Błędy RMSE (kg): {rmse_scores_dt}")
-print(f"Średni błąd (RMSE): {rmse_scores_dt.mean():.4f} kg")
+logging.info("--- WYNIKI DECISION TREE ---")
+logging.info(f"Błędy RMSE (kg): {rmse_scores_dt}")
+logging.info(f"Średni błąd (RMSE): {rmse_scores_dt.mean():.4f} kg")
 
 error_percentage_dt = (rmse_scores_dt.mean() / df_clean['waga_czczo'].mean()) * 100
-print(f"Procentowy błąd: {error_percentage_dt:.2f}%")
+logging.info(f"Procentowy błąd: {error_percentage_dt:.2f}%")
 
 
 # -----przygtotowanie modeli do symulacji------------
+logging.info("Trenowanie modeli końcowych...")
 model_xgb.fit(X, y, verbose=False)
 model_lgb.fit(X, y)
 model_cat.fit(X, y)
@@ -305,6 +294,7 @@ pipeline_ridge.fit(X, y)
 pipeline_svm.fit(X, y)
 pipeline_knn.fit(X, y)
 model_dt.fit(X, y)
+logging.info("Modele zostały pomyślnie wytrenowane.")
 
 
 # -------------symulacja zmiany wagi--------------
@@ -350,37 +340,27 @@ kalk_kroki = 14000
 
 # --- predykcja---
 
-wyniki_xgb = symulacja_wagi(model_xgb, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio,
-                            kalk_silowy, kalk_kroki)
-wyniki_lgb = symulacja_wagi(model_lgb, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio,
-                            kalk_silowy, kalk_kroki)
-wyniki_cat = symulacja_wagi(model_cat, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio,
-                            kalk_silowy, kalk_kroki)
-wyniki_rf = symulacja_wagi(model_rf, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio,
-                           kalk_silowy, kalk_kroki)
-wyniki_lr = symulacja_wagi(pipeline_lr, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio,
-                           kalk_silowy, kalk_kroki)
-wyniki_en = symulacja_wagi(pipeline_en, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio,
-                           kalk_silowy, kalk_kroki)
-wyniki_ridge = symulacja_wagi(pipeline_ridge, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone,
-                              kalk_cardio, kalk_silowy, kalk_kroki)
-wyniki_svm = symulacja_wagi(pipeline_svm, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio,
-                            kalk_silowy, kalk_kroki)
-wyniki_knn = symulacja_wagi(pipeline_knn, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio,
-                            kalk_silowy, kalk_kroki)
-wyniki_dt = symulacja_wagi(model_dt, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio,
-                           kalk_silowy, kalk_kroki)
+wyniki_xgb = symulacja_wagi(model_xgb, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio, kalk_silowy, kalk_kroki)
+wyniki_lgb = symulacja_wagi(model_lgb, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio, kalk_silowy, kalk_kroki)
+wyniki_cat = symulacja_wagi(model_cat, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio, kalk_silowy, kalk_kroki)
+wyniki_rf = symulacja_wagi(model_rf, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio, kalk_silowy, kalk_kroki)
+wyniki_lr = symulacja_wagi(pipeline_lr, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio, kalk_silowy, kalk_kroki)
+wyniki_en = symulacja_wagi(pipeline_en, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio, kalk_silowy, kalk_kroki)
+wyniki_ridge = symulacja_wagi(pipeline_ridge, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio, kalk_silowy, kalk_kroki)
+wyniki_svm = symulacja_wagi(pipeline_svm, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio, kalk_silowy, kalk_kroki)
+wyniki_knn = symulacja_wagi(pipeline_knn, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio, kalk_silowy, kalk_kroki)
+wyniki_dt = symulacja_wagi(model_dt, dni_prognozy, waga_start, kalk_kcal, kalk_bialko, kalk_spalone, kalk_cardio, kalk_silowy, kalk_kroki)
 
 # --- porownanie --
-print("\n------POROWNANIE------")
-print(f"Waga startowa: {waga_start} kg")
-print(f"XGBOOST po {dni_prognozy} dniach: {wyniki_xgb[-1]:.2f} kg | Różnica: {(wyniki_xgb[-1] - waga_start):.2f} kg")
-print(f"LIGHTGBM po {dni_prognozy} dniach: {wyniki_lgb[-1]:.2f} kg | Różnica: {(wyniki_lgb[-1] - waga_start):.2f} kg")
-print(f"CATBOOST po {dni_prognozy} dniach: {wyniki_cat[-1]:.2f} kg | Różnica: {(wyniki_cat[-1] - waga_start):.2f} kg")
-print(f"RANDOMFOREST po {dni_prognozy} dniach: {wyniki_rf[-1]:.2f} kg | Różnica: {(wyniki_rf[-1] - waga_start):.2f} kg")
-print(f"LINEAR REGRESSION po {dni_prognozy} dniach: {wyniki_lr[-1]:.2f} kg | Różnica: {(wyniki_lr[-1] - waga_start):.2f} kg")
-print(f"ELASTIC NET po {dni_prognozy} dniach: {wyniki_en[-1]:.2f} kg | Różnica: {(wyniki_en[-1] - waga_start):.2f} kg")
-print(f"RIDGE REGRESSION po {dni_prognozy} dniach: {wyniki_ridge[-1]:.2f} kg | Różnica: {(wyniki_ridge[-1] - waga_start):.2f} kg")
-print(f"SVM po {dni_prognozy} dniach: {wyniki_svm[-1]:.2f} kg | Różnica: {(wyniki_svm[-1] - waga_start):.2f} kg")
-print(f"KNN po {dni_prognozy} dniach: {wyniki_knn[-1]:.2f} kg | Różnica: {(wyniki_knn[-1] - waga_start):.2f} kg")
-print(f"DECISION TREE po {dni_prognozy} dniach: {wyniki_dt[-1]:.2f} kg | Różnica: {(wyniki_dt[-1] - waga_start):.2f} kg")
+logging.info("------ PORÓWNANIE KOŃCOWE ------")
+logging.info(f"Waga startowa: {waga_start:.2f} kg")
+logging.info(f"XGBOOST po {dni_prognozy} dniach: {wyniki_xgb[-1]:.2f} kg | Różnica: {(wyniki_xgb[-1] - waga_start):.2f} kg")
+logging.info(f"LIGHTGBM po {dni_prognozy} dniach: {wyniki_lgb[-1]:.2f} kg | Różnica: {(wyniki_lgb[-1] - waga_start):.2f} kg")
+logging.info(f"CATBOOST po {dni_prognozy} dniach: {wyniki_cat[-1]:.2f} kg | Różnica: {(wyniki_cat[-1] - waga_start):.2f} kg")
+logging.info(f"RANDOMFOREST po {dni_prognozy} dniach: {wyniki_rf[-1]:.2f} kg | Różnica: {(wyniki_rf[-1] - waga_start):.2f} kg")
+logging.info(f"LINEAR REGRESSION po {dni_prognozy} dniach: {wyniki_lr[-1]:.2f} kg | Różnica: {(wyniki_lr[-1] - waga_start):.2f} kg")
+logging.info(f"ELASTIC NET po {dni_prognozy} dniach: {wyniki_en[-1]:.2f} kg | Różnica: {(wyniki_en[-1] - waga_start):.2f} kg")
+logging.info(f"RIDGE REGRESSION po {dni_prognozy} dniach: {wyniki_ridge[-1]:.2f} kg | Różnica: {(wyniki_ridge[-1] - waga_start):.2f} kg")
+logging.info(f"SVM po {dni_prognozy} dniach: {wyniki_svm[-1]:.2f} kg | Różnica: {(wyniki_svm[-1] - waga_start):.2f} kg")
+logging.info(f"KNN po {dni_prognozy} dniach: {wyniki_knn[-1]:.2f} kg | Różnica: {(wyniki_knn[-1] - waga_start):.2f} kg")
+logging.info(f"DECISION TREE po {dni_prognozy} dniach: {wyniki_dt[-1]:.2f} kg | Różnica: {(wyniki_dt[-1] - waga_start):.2f} kg")
